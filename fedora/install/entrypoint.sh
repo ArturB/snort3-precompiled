bb2a2087647c8bb25642c130843e95c15297923f
Artur M. Brodzki <artur@brodzki.org>
2019-11-27 18:45:34 +0100

    reorganize files

--- fedora/install/entrypoint.sh
+++ fedora/install/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -e
+
 # If specified, keep container open, allowing user to log in with bash
 if [[ $1 == "--keep" ]]; then 
 echo -e \
@@ -8,10 +11,9 @@ You may now log in into the container with the following command:\
 docker exec -it $(docker ps -q) /bin/bash' ; sleep infinity
 fi
 
-# Copy the RPM package from dist directory and install snort
-cp -v $( find $DIST_DIR -name *$FEDORA_VERSION*.rpm ) / 
-dnf install -y /snort3* 
+# Install snort
+dnf install -y $DIST_DIR/snort3*$DIST_NAME*.rpm
 
 # Basic installation check
-snort --daq-dir=/usr/lib64/daq --daq-list
+snort --daq-dir=$TARGET_LIBRARY_PATH/daq --daq-list
 snort -V
